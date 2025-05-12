e473836f0408fa431744a3d5331f7b2dfa4faf85
Sony <138565882+dechathon518@users.noreply.github.com>
2025-05-12 22:16:35 +0700

    Delete unnecessary

--- javascript/animation.js
+++
@@ -1,4 +0,0 @@
-export function Animationfinish(element){
-    const animationPo = element.getAnimations().map(animation => animation.finished)
-    return Promise.allSettled(animationPo);
-} //all animation resolve
--- javascript/invitePopup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Show all popups on page load
-  const popups = document.querySelectorAll(".invite-popup");
-  popups.forEach((popup) => {
-    popup.style.display = "block";
-  });
-});
